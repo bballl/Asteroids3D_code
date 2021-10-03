31e79127f23ff5a08a09f34660ff883707be545e
TikhonovGleb <bball2011@yandex.ru>
2021-10-03 23:01:41 +0300

    Homework3

---
+++ InputController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Asteroids
+{
+    internal class InputController : MonoBehaviour
+    {
+        private float _jumpButton;
+
+        public float GetHorizontal()
+        {
+            return Input.GetAxis("Horizontal");
+        }
+
+        public float GetVertical()
+        {
+            return Input.GetAxis("Vertical");
+        }
+
+        public bool GetJumpButton()
+        {
+            _jumpButton = Input.GetAxis("Jump");
+
+            if (_jumpButton > 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+        
+        public bool GetFireButton()
+        {
+            return Input.GetButtonDown("Fire1");
+        }
+    }
+}
+
+
